20576af14aa6d9350a085bbfb7cdcca4f2309b37
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-23 19:28:18 +0200

    Create credit (to mention emoji author)

---
+++ src/components/credit.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Credit = ({ children }) => (
+  <div className="mike">emoji by <a href="https://codepen.io/mikemang" target="_blank" rel="noopener noreferrer"> Mike Mangialardi </a></div>
+)
+
+export default Credit
