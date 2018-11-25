d6a859ae15eecb0bac0206e3f71e2e3f54ca50c3
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-25 11:45:37 +0200

    Edit header styling, add links to about and recipes pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,36 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 import './layout.css'
 
-const Header = ({ siteTitle }) => (
-  <div id="header"
-    style={{
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0, color: 'white' }}>
-        <Link
-          to="/"
-          style={{
-            color: '#b8defa',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+library.add(faHome)
+
+const ItemLink = (props) => (
+  <li>
+    <Link to={props.to} style={{color: '#b8defa', fontSize: '3.5vw', padding: '0 3vw 0 0'}}>
+      {props.children}
+    </Link>
+  </li>
+);
+
+const Header = () => (
+  <div id="header">
+      <ul id="menu" style={{listStyleType: 'none',  margin: '0', display: 'flex'}}> 
+        <ItemLink to="/">
+          <FontAwesomeIcon icon="home"/> 
+        </ItemLink>
+        <ItemLink to="/about">
+          about
+        </ItemLink>
+        <ItemLink to="/recipes">
+          recipes
+        </ItemLink>
+      </ul>
   </div>
 )
 
 export default Header
+
+ 
