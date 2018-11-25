29bc22dd034e22d5bc932b5271af65469d92ba41
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-25 11:44:11 +0200

    Edit description and keywords, remove styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,21 +22,15 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'A recipe blog' },
+            { name: 'keywords', content: 'gatsby, react, cookbook, recipes' },
           ]}
         >
           <html lang="en" />
         </Helmet>
+
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div className="layout">
           {children}
         </div>
       </>
