2bbdf41a22f3548e3acb0ad92a805be3d2a722ca
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-26 16:12:15 +0200

    Edit styles (move styles to a variable)

--- src/components/header.js
+++ src/components/header.js
@@ -7,9 +7,17 @@ import './layout.css'
 
 library.add(faHome)
 
+const menuStyle = {
+  listStyleType: 'none', 
+  margin: '0', 
+  display: 'flex', 
+  justifyContent:'space-between', 
+  paddingRight:'14vw'
+}
+
 const ItemLink = (props) => (
   <li>
-    <Link to={props.to} style={{color: '#b8defa', fontSize: '3.5vw', padding: '0 3vw 0 0'}}>
+    <Link to={props.to} style={{color: '#b8defa', fontSize: '3.5vw'}}>
       {props.children}
     </Link>
   </li>
@@ -17,9 +25,9 @@ const ItemLink = (props) => (
 
 const Header = () => (
   <div id="header">
-      <ul id="menu" style={{listStyleType: 'none',  margin: '0', display: 'flex'}}> 
+      <ul id="menu" style={menuStyle}> 
         <ItemLink to="/">
-          <FontAwesomeIcon icon="home"/> 
+          <FontAwesomeIcon icon="home" style={{fontSize: '2.8vw', paddingBottom: '0.3vh'}}/> 
         </ItemLink>
         <ItemLink to="/about">
           about
